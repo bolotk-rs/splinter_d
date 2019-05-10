4a1c5aef3afd4aa498ec0315e03d6c6673e6f39b
Bolot <bolot.kadyraliev@redshelf.com>
2019-05-10 15:22:20 -0500

    Added logout action to webdriver

--- splinter/driver/webdriver/__init__.py
+++ splinter/driver/webdriver/__init__.py
@@ -219,7 +219,7 @@ class BaseWebDriver(DriverAPI):
                 return True
         return False
 
-    def sign_in(self, user_name, password):
+    def login(self, user_name, password):
         from splinter.exceptions import ElementDoesNotExist
 
         try:
@@ -239,6 +239,16 @@ class BaseWebDriver(DriverAPI):
 
         except ElementDoesNotExist:
             print("You already signed in to: " + self.title)
+
+    def logout(self):
+        _el = self.is_element_visible_by_xpath("//button[@*='User Menu Toggle']", wait_time=2)
+
+        if _el:
+            self.click_link_by_xpath("//button[@*='User Menu Toggle']")
+        else:
+            self.click_link_by_xpath("//div[@class='rs-navbar-nav-item']")
+
+        self.click_link_by_xpath("//*[contains(text(), 'Log Out')]")        
 
     def is_element_not_visible(self, finder, selector, wait_time=None):
         wait_time = wait_time or self.wait_time
